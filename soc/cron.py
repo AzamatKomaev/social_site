1c4dcf90c2afba8716b78746c3dd544ddcad8520
Азамат Комаев <azamatkomaev15@gmail.com>
2021-10-07 07:53:22 +0300

    Add new test

--- soc/cron.py
+++ soc/cron.py
@@ -9,14 +9,10 @@ print(f"cron was started at {datetime.now()}")
 try:
     conn = sqlite3.connect("/home/azamat/Рабочий стол/Django/social/db.sqlite3") #Важно! Вставляем относительный путь до базы данных
     cur = conn.cursor()
-
     """Удаляем пользователей с меткой is_active == False"""
     cur.execute("DELETE FROM auth_user WHERE date_joined < datetime('now', 'localtime', '-1 day') AND is_active = 0;")
-    print("Execute command")
     conn.commit()
-    print("Commit command")
 
     conn.close()
-    print("close connect")
 except Exception as e:
     print(e)
--- soc/tests.py
+++ soc/tests.py
@@ -1,14 +1,25 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 
-from .models import Post, Token
+from .service.user_service import CreationUser
+from .models import Token
+
 
 
 class SocialTest(TestCase):
-    def setUp(self):
-        User.objects.create(username="test_user", password="test_password")
-        Post.objects.create(title="hi it is test", text="yeeaaaaaah", user_id=1)
+    """Данные для регистраций тестового пользователя."""
+    data_for_reg_user = {
+        'username': "Test_User",
+        'email': "testusermail@gmail.com",
+        'password1': "testpassword2"
+    }
 
-    def test_first_name_max_length(self):
-        post = Post.objects.get(id=1)
-        self.assertEqual(post.id, 1)
+    def setUp(self):
+        user = CreationUser(self.data_for_reg_user)
+        user.send_message_with_code()
+
+    def test_token_on_valid(self):
+        """Тестим токен на валидность."""
+        test_user = User.objects.get()
+        test_user.groups.all()
+        self.assertEqual(test_user.username, "Test_User")
